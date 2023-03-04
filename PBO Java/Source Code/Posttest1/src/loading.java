c08ca0bb292c3a2b3e99ee9e064ac389f8b9a619
dimsgusti <90297502+dimsgusti@users.noreply.github.com>
2023-03-05 00:26:57 +0800

    Delete PBO Java/Source Code/Posttest1/src directory

--- PBO Java/Source Code/Posttest1/src/loading.java
+++
@@ -1,9 +0,0 @@
-public class loading {
-    public static void wait(int ms){
-        try{
-            Thread.sleep(ms);
-        } catch(InterruptedException ex){
-            Thread.currentThread().interrupt();
-        }
-    }    
-}
